ef72a395ab71c3dc7ba22433d06f0ec57b8982e9
Sgominator <mogaleobakeng68@gmail.com>
2023-07-14 15:25:32 +0200

    added new files and made some buttons function

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express')
 const app = express()
-
+const ejs = require("ejs")
 
 app.use(express.urlencoded({
     extended: true
 }))
+app.set('view engine', 'ejs')
 app.use(express.static("public"))
 
 
 app.get('/', function (req, res) {
 
-  res.sendFile(__dirname+"/index.html")
+  res.render('index.ejs')
 })
 
 app.post('/', function (req,res) {
+  // const clothingCategory  ; 
 
-  res.redirect('/shop')
+  res.redirect('/shop/'+ clothingCategory)
 })
-app.get('/shop', function(req, res) {
 
-  res.sendFile(__dirname+"shop.html")
+app.get('/login', function(req,res){
+  res.render('login.ejs')
+})
+
+app.get('/register', function(req,res){
+  res.render('register.ejs')
+})
+//shop req and res
+app.get('/shop/hoodies', function(req, res) {
+
+  res.render('shop.ejs')
 })
 
 app.listen(3000)
